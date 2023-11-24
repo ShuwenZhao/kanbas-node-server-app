0e59fb16142329c975c11366e71924768acda6f3
Shuwen Zhao <shuwenzhao@yahoo.com>
2023-11-23 16:34:04 -0800

    connect nodejs to mongo atlas

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 const app = express();
 
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
+
+
 app.use(
   cors({
     credentials: true,
